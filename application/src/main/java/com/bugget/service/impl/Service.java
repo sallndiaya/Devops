dca19a8903363e26c7788c15e07b6ab32d0edf4b
EspoirNorbert <espoirnorbert@gmail.com>
2022-07-13 04:26:13 +0200

    Mise a jour du fichier service

--- application/src/main/java/com/bugget/service/impl/Service.java
+++ application/src/main/java/com/bugget/service/impl/Service.java
@@ -1,8 +1,31 @@
 package com.bugget.service.impl;
 
+import com.bugget.dao.IDao;
+import com.bugget.dao.impl.HibernateDao;
+import com.bugget.entities.User;
+import com.bugget.service.IService;
 
-public class Service  {
+public class Service implements IService {
 
+	private static IDao dao = HibernateDao.getCurrentInstance();
+	private static IService instance = new Service();
 
+	private Service() {}
 
+	public static IService getCurrentInstance() {
+		return instance;
+	}
+	
+	@Override
+	public boolean creerCompte(User user) {
+			dao.save(user);
+			return true;
+	}
+
+	@Override
+	public User authenticate(User user) {
+		return null;
+	}
+
+	
 }
